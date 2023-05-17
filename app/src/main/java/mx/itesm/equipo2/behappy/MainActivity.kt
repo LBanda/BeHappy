47bfe2c2379a3736283fdc23d9a6815115125ba9
LBanda <A01610329@itesm.mx>
2023-05-17 12:22:14 -0600

    pantalla login
    desarrollo de la pantalla de login con google

--- app/src/main/java/mx/itesm/equipo2/behappy/MainActivity.kt
+++ app/src/main/java/mx/itesm/equipo2/behappy/MainActivity.kt
@@ -1,11 +1,34 @@
 package mx.itesm.equipo2.behappy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.firebase.ui.auth.AuthUI
+import mx.itesm.equipo2.behappy.databinding.ActivityLoginBinding
+import mx.itesm.equipo2.behappy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.btnCerrarSesion.setOnClickListener {
+            AuthUI.getInstance().signOut(this).addOnCompleteListener {
+                finish()
+
+                val intLogin = Intent(this, LoginActivity::class.java)
+                startActivity(intLogin)
+            }
+        }
     }
 }
