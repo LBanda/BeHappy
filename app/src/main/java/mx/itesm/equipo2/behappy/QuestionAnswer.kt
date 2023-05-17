8049b4af67446fcdbeab67d1ad55078c65e3104c
LBanda <A01610329@itesm.mx>
2023-05-17 15:57:18 -0600

    cuestionario
    vista de cuestionario

---
+++ app/src/main/java/mx/itesm/equipo2/behappy/QuestionAnswer.kt
@@ -0,0 +1,16 @@
+package mx.itesm.equipo2.behappy
+
+object QuestionAnswer {
+    var question = arrayOf(
+        "Cómo te sientes hoy?",
+        "Qué tal tu día?"
+    )
+    var choices = arrayOf(
+        arrayOf("Muy mal", "Maso", "Buen", "Feliz"),
+        arrayOf("Java", "Kotlin", "Notepad", "Python")
+    )
+    var correctAnswers = arrayOf(
+        "Feliz",
+        "Notepad"
+    )
+}
