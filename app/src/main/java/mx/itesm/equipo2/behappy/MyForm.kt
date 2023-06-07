fc758efd45534d185c1cdd9afcbfc07bc1484271
LBanda <A01610329@itesm.mx>
2023-06-07 13:42:38 -0600

    form ya con menu lateral

--- app/src/main/java/mx/itesm/equipo2/behappy/MyForm.kt
+++ app/src/main/java/mx/itesm/equipo2/behappy/MyForm.kt
@@ -8,9 +8,24 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.ImageView
 import android.widget.Toast
+import com.google.android.material.navigation.NavigationView
+
+import android.content.Intent
+import mx.itesm.equipo2.behappy.databinding.ActivityMyFormBinding
+import android.content.res.Configuration
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.firebase.ui.auth.AuthUI
+
+class MyForm : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var binding: ActivityMyFormBinding
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
 
-class MyForm : AppCompatActivity(), View.OnClickListener {
     private lateinit var questionTextView: TextView
     private lateinit var scoreTextView: TextView
     private lateinit var veredictTextView: TextView
@@ -38,7 +53,27 @@ class MyForm : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_form)
+        binding = ActivityMyFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
 
         questionTextView = findViewById(R.id.questionTextView)
         scoreTextView = findViewById(R.id.scoreTextView)
@@ -56,6 +91,54 @@ class MyForm : AppCompatActivity(), View.OnClickListener {
 
         veredictTextView.visibility = View.GONE
         updateQuestion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.btnCerrarSesion.setOnClickListener {
+            AuthUI.getInstance().signOut(this).addOnCompleteListener {
+                finish()
+
+                val intLogin = Intent(this, LoginActivity::class.java)
+                startActivity(intLogin)
+            }
+        }
+
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.activity_inicio -> {val intInicio= Intent(this, MainActivity::class.java)
+                startActivity(intInicio)}
+            R.id.activity_profile2 -> {val intPerfil = Intent(this, Profile::class.java)
+                startActivity(intPerfil)}
+            R.id.activity_my_form -> {val intForm = Intent(this, MyForm::class.java)
+                startActivity(intForm)}
+            R.id.activity_contacto -> {val intContacto = Intent(this, Contacto::class.java)
+                startActivity(intContacto)}
+        }
+
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onClick(view: View) {
